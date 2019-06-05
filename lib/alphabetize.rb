a1045b62d462a5a9d6bb1f25363e12ab047f6c38
Alexander Riccio <test35965+learn@gmail.com>
2019-06-05 10:26:56 -0400

    Solve lab, hopefully

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,4 @@
+require "pry"
 ESPERANTO_MAP = {
   "a" => 0,
   "b" => 1,
@@ -26,14 +27,15 @@ ESPERANTO_MAP = {
   "u" => 24,
   "ŭ" => 25,
   "v" => 26,
-  "z" => 27
+  "z" => 27,
+  " " => 28
 }
 
 def swap_sort(phrase_a, phrase_b)
   i = 0
   j = 0
   while (i < phrase_a.length) && (j < phrase_b.length) do
-    binding.pry
+    #binding.pry
     if ESPERANTO_MAP[phrase_a[i]] > ESPERANTO_MAP[phrase_b[j]]
       return [phrase_a, phrase_b]
     elsif ESPERANTO_MAP[phrase_a[i]] < ESPERANTO_MAP[phrase_b[j]]
@@ -50,14 +52,56 @@ def alphabetize(arr)
     #arr.sort{ |phrase_a, phrase_b| (ESPERANTO_MAP[phrase_a[0]] <=> ESPERANTO_MAP[phrase_b[0]])}
     #arr.sort{ |phrase_a, phrase_b| (ESPERANTO_MAP[phrase_a[1]] <=> ESPERANTO_MAP[phrase_b[1]])}
     #arr.sort
-  10.times do
-    i = 0
-    while i < (arr.length() -2) do
-      move = swap_sort(arr[i], arr[i+1])
-      arr[i] = move[0]
-      arr[i+1] = move[1]
-      i += 1
+  split_phrases = []
+  arr.map do |phrase|
+    split_phrases << phrase.split("")
+    #binding.pry
+  end
+  split_phrases.map do |each_phrase|
+    #binding.pry
+    each_phrase.map! do |esperanto_char|
+      replacement_char_value = ESPERANTO_MAP[esperanto_char]
+      #binding.pry
+      if replacement_char_value == nil
+        binding.pry
+      end
+      esperanto_char = replacement_char_value
     end
   end
-  arr
+  binding.pry
+
+  split_phrases.sort!()
+
+  binding.pry
+
+  split_phrases.map do |each_phrase|
+    #binding.pry
+    each_phrase.map! do |esperanto_char_ord|
+      replacement_char_value = ESPERANTO_MAP.invert[esperanto_char_ord]
+      #binding.pry
+      if replacement_char_value == nil
+        binding.pry
+      end
+      esperanto_char_ord = replacement_char_value
+    end
+  end
+  binding.pry
+
+  split_phrases.map! do |each_phrase|
+    each_phrase.join
+  end
+  binding.pry
+
+  # 10.times do
+  #   i = 0
+  #   while i < (arr.length() -1) do
+  #     move = swap_sort(arr[i], arr[i+1])
+  #     arr[i] = move[0]
+  #     arr[i+1] = move[1]
+  #     i += 1
+  #   end
+  # end
+  # arr
+
+  split_phrases
 end
